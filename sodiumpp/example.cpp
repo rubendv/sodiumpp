188bffdbf9315c0f16fbb12fafa6eeaa1cf846f7
Ruben De Visscher <ruben.de.visscher@gmail.com>
2014-08-04 09:40:08 +0200

    More output in example

--- sodiumpp/example.cpp
+++ sodiumpp/example.cpp
@@ -30,16 +30,26 @@ int main(int argc, const char ** argv) {
     box_secret_key sk_client;
     box_secret_key sk_server;
 
-    // Uses predefined nonce type with 64-bit sequential counter and constant random bytes for the rest
+    std::cout << "Client key: " << sk_client << std::endl;
+    std::cout << "Server key: " << sk_server << std::endl;
+    std::cout << std::endl;
+
+    // Uses predefined nonce type with 64-bit sequential counter 
+    // and constant random bytes for the rest
     boxer<nonce64> client_boxer(sk_server.pk, sk_client);
     unboxer<nonce64> server_unboxer(sk_client.pk, sk_server, client_boxer.get_nonce_constant());
 
     encoded_bytes boxed = client_boxer.box("Hello, world!\n");
+    std::cout << "Nonce (hex): " << client_boxer.get_nonce().get(encoding::hex).bytes << std::endl;
+    std::cout << "Boxed message (z85): " << boxed.to(encoding::z85).bytes << std::endl;
     std::string unboxed = server_unboxer.unbox(boxed);
-    std::cout << unboxed;
+    std::cout << "Unboxed message: " << unboxed;
+    std::cout << std::endl;
 
     boxed = client_boxer.box("From sodiumpp!\n");
     unboxed = server_unboxer.unbox(boxed);
-    std::cout << unboxed;
+    std::cout << "Nonce (hex): " << client_boxer.get_nonce().get(encoding::hex).bytes << std::endl;
+    std::cout << "Boxed message (z85): " << boxed.to(encoding::z85).bytes << std::endl;
+    std::cout << "Unboxed message: " << unboxed;
     return 0;
 }
